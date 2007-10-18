789a6682f8639d79d419036369a4d6fb36fc2de9
Fred Sauer <fredsa@gmail.com>
2007-10-18 21:43:19 +0000

    add license information
    git-svn-id: https://gwt-log.googlecode.com/svn/trunk@14 dec3d7ae-b23b-0410-ba11-938393925c7a

--- Log/src/com/allen_sauer/gwt/log/client/DivLogger.java
+++ Log/src/com/allen_sauer/gwt/log/client/DivLogger.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2007 Fred Sauer
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.allen_sauer.gwt.log.client;
 
 import com.google.gwt.user.client.Command;
@@ -36,8 +51,7 @@ public class DivLogger extends AbstractLogger {
     header.addStyleName(STYLE_LOG_HEADER);
     debugTable.setWidget(0, 0, header);
     debugTable.setWidget(1, 0, scrollPanel);
-    debugTable.getCellFormatter().setHorizontalAlignment(0, 0,
-        HasHorizontalAlignment.ALIGN_CENTER);
+    debugTable.getCellFormatter().setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_CENTER);
 
     scrollPanel.setWidget(debugTextArea);
 
@@ -57,8 +71,7 @@ public class DivLogger extends AbstractLogger {
         if (dragging) {
           int absX = x + debugTable.getAbsoluteLeft();
           int absY = y + debugTable.getAbsoluteTop();
-          RootPanel.get().setWidgetPosition(debugTable, absX - dragStartX,
-              absY - dragStartY);
+          RootPanel.get().setWidgetPosition(debugTable, absX - dragStartX, absY - dragStartY);
         }
       }
 
@@ -98,8 +111,7 @@ public class DivLogger extends AbstractLogger {
     message = message.replaceAll("<", "&lt;");
     message = message.replaceAll(">", "&gt;");
     message = message.replaceAll("\r\n|\r|\n", "<BR>");
-    setDebugText(debugText + "<div style='color:" + getColor(logLevel) + ";'>"
-        + message + "</div>");
+    setDebugText(debugText + "<div style='color: " + getColor(logLevel) + ";'>" + message + "</div>");
   }
 
   public void moveTo(int x, int y) {
