4356300e76456ae5478b809f1e8e5d919baf1964
Fred Sauer <fredsa@gmail.com>
2008-01-05 00:00:38 +0000

    New javadoc
    git-svn-id: https://gwt-log.googlecode.com/svn/trunk/Log@45 dec3d7ae-b23b-0410-ba11-938393925c7a

--- src/com/allen_sauer/gwt/log/client/Log.java
+++ src/com/allen_sauer/gwt/log/client/Log.java
@@ -38,38 +38,185 @@ public final class Log {
     impl.clear();
   }
 
+  /**
+   * Log a <code>DEBUG</code> level message with no
+   * exception information.
+   * 
+   * @see Log#debug(String, JavaScriptObject)
+   * @see Log#debug(String, Throwable)
+   * 
+   * @param message the text to be logged
+   */
   public static void debug(String message) {
     debug(message, (Throwable) null);
   }
 
+  /**
+   * Log a <code>DEBUG</code> level message from within
+   * a JSNI try/catch block, e.g.
+   * <pre>
+   *   private native void jsniTryCatchExample()
+   *   /&#42;-{
+   *     try {
+   *       // throws exception
+   *       non_existant_variable.non_existant_method();
+   *     } catch(e) {
+   *       &#64;com.allen_sauer.gwt.log.client.Log::debug(Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)("Caught JSNI Exception", e);
+   *     }
+   *   }-&#42;/;
+   * </pre>   
+   * 
+   * @see Log#debug(String)
+   * @see Log#debug(String, Throwable)
+   * 
+   * @param message the text to be logged
+   * @param e the native JavaScript exception object
+   */
   public static void debug(String message, JavaScriptObject e) {
     impl.debug(message, e);
   }
 
+  /**
+   * Log a <code>DEBUG</code> level message from within
+   * a Java try/catch block, e.g.
+   * <pre>
+   *   private native void javaTryCatchExample() {
+   *     try {
+   *       throw new RuntimeException();
+   *     } catch(e) {
+   *       Log.debug("Caught Java Exception", e);
+   *     }
+   *   }
+   * </pre>   
+   * 
+   * @see Log#debug(String)
+   * @see Log#debug(String, JavaScriptObject)
+   * 
+   * @param message the text to be logged
+   * @param e the native JavaScript exception object
+   */
   public static void debug(String message, Throwable e) {
     impl.debug(message, e);
   }
 
+  /**
+   * Log a <code>ERROR</code> level message with no
+   * exception information.
+   * 
+   * @see Log#error(String, JavaScriptObject)
+   * @see Log#error(String, Throwable)
+   * 
+   * @param message the text to be logged
+   */
   public static void error(String message) {
     error(message, (Throwable) null);
   }
 
+  /**
+   * Log a <code>ERROR</code> level message from within
+   * a JSNI try/catch block, e.g.
+   * <pre>
+   *   private native void jsniTryCatchExample()
+   *   /&#42;-{
+   *     try {
+   *       // throws exception
+   *       non_existant_variable.non_existant_method();
+   *     } catch(e) {
+   *       &#64;com.allen_sauer.gwt.log.client.Log::error(Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)("Caught JSNI Exception", e);
+   *     }
+   *   }-&#42;/;
+   * </pre>   
+   * 
+   * @see Log#error(String)
+   * @see Log#error(String, Throwable)
+   * 
+   * @param message the text to be logged
+   * @param e the native JavaScript exception object
+   */
   public static void error(String message, JavaScriptObject e) {
     impl.error(message, e);
   }
 
+  /**
+   * Log a <code>ERROR</code> level message from within
+   * a Java try/catch block, e.g.
+   * <pre>
+   *   private native void javaTryCatchExample() {
+   *     try {
+   *       throw new RuntimeException();
+   *     } catch(e) {
+   *       Log.error("Caught Java Exception", e);
+   *     }
+   *   }
+   * </pre>   
+   * 
+   * @see Log#error(String)
+   * @see Log#error(String, JavaScriptObject)
+   * 
+   * @param message the text to be logged
+   * @param e the native JavaScript exception object
+   */
   public static void error(String message, Throwable e) {
     impl.error(message, e);
   }
 
+  /**
+   * Log a <code>FATAL</code> level message with no
+   * exception information.
+   * 
+   * @see Log#fatal(String, JavaScriptObject)
+   * @see Log#fatal(String, Throwable)
+   * 
+   * @param message the text to be logged
+   */
   public static void fatal(String message) {
     fatal(message, (Throwable) null);
   }
 
+  /**
+   * Log a <code>FATAL</code> level message from within
+   * a JSNI try/catch block, e.g.
+   * <pre>
+   *   private native void jsniTryCatchExample()
+   *   /&#42;-{
+   *     try {
+   *       // throws exception
+   *       non_existant_variable.non_existant_method();
+   *     } catch(e) {
+   *       &#64;com.allen_sauer.gwt.log.client.Log::fatal(Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)("Caught JSNI Exception", e);
+   *     }
+   *   }-&#42;/;
+   * </pre>   
+   * 
+   * @see Log#fatal(String)
+   * @see Log#fatal(String, Throwable)
+   * 
+   * @param message the text to be logged
+   * @param e the native JavaScript exception object
+   */
   public static void fatal(String message, JavaScriptObject e) {
     impl.fatal(message, e);
   }
 
+  /**
+   * Log a <code>FATAL</code> level message from within
+   * a Java try/catch block, e.g.
+   * <pre>
+   *   private native void javaTryCatchExample() {
+   *     try {
+   *       throw new RuntimeException();
+   *     } catch(e) {
+   *       Log.fatal("Caught Java Exception", e);
+   *     }
+   *   }
+   * </pre>   
+   * 
+   * @see Log#fatal(String)
+   * @see Log#fatal(String, JavaScriptObject)
+   * 
+   * @param message the text to be logged
+   * @param e the native JavaScript exception object
+   */
   public static void fatal(String message, Throwable e) {
     impl.fatal(message, e);
   }
@@ -110,14 +257,63 @@ public final class Log {
     return impl.getLoggerSystem();
   }
 
+  /**
+   * Log a <code>INFO</code> level message with no
+   * exception information.
+   * 
+   * @see Log#info(String, JavaScriptObject)
+   * @see Log#info(String, Throwable)
+   * 
+   * @param message the text to be logged
+   */
   public static void info(String message) {
     info(message, (Throwable) null);
   }
 
+  /**
+   * Log a <code>INFO</code> level message from within
+   * a JSNI try/catch block, e.g.
+   * <pre>
+   *   private native void jsniTryCatchExample()
+   *   /&#42;-{
+   *     try {
+   *       // throws exception
+   *       non_existant_variable.non_existant_method();
+   *     } catch(e) {
+   *       &#64;com.allen_sauer.gwt.log.client.Log::info(Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)("Caught JSNI Exception", e);
+   *     }
+   *   }-&#42;/;
+   * </pre>   
+   * 
+   * @see Log#info(String)
+   * @see Log#info(String, Throwable)
+   * 
+   * @param message the text to be logged
+   * @param e the native JavaScript exception object
+   */
   public static void info(String message, JavaScriptObject e) {
     impl.info(message, e);
   }
 
+  /**
+   * Log a <code>INFO</code> level message from within
+   * a Java try/catch block, e.g.
+   * <pre>
+   *   private native void javaTryCatchExample() {
+   *     try {
+   *       throw new RuntimeException();
+   *     } catch(e) {
+   *       Log.info("Caught Java Exception", e);
+   *     }
+   *   }
+   * </pre>   
+   * 
+   * @see Log#info(String)
+   * @see Log#info(String, JavaScriptObject)
+   * 
+   * @param message the text to be logged
+   * @param e the native JavaScript exception object
+   */
   public static void info(String message, Throwable e) {
     impl.info(message, e);
   }
@@ -150,18 +346,73 @@ public final class Log {
     impl.setCurrentLogLevel(level);
   }
 
+  /**
+   * Installs an UncaughtExceptionHandler that will trap and
+   * log <code>FATAL</code> messages, but only if <i>both</i>
+   * the compile time and runtime <code>log_level</code>
+   * is set to <code>FATAL</code> or lower.
+   */
   public static void setUncaughtExceptionHandler() {
     impl.setUncaughtExceptionHandler();
   }
 
+  /**
+   * Log a <code>WARN</code> level message with no
+   * exception information.
+   * 
+   * @see Log#warn(String, JavaScriptObject)
+   * @see Log#warn(String, Throwable)
+   * 
+   * @param message the text to be logged
+   */
   public static void warn(String message) {
     warn(message, (Throwable) null);
   }
 
+  /**
+   * Log a <code>WARN</code> level message from within
+   * a JSNI try/catch block, e.g.
+   * <pre>
+   *   private native void jsniTryCatchExample()
+   *   /&#42;-{
+   *     try {
+   *       // throws exception
+   *       non_existant_variable.non_existant_method();
+   *     } catch(e) {
+   *       &#64;com.allen_sauer.gwt.log.client.Log::warn(Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;)("Caught JSNI Exception", e);
+   *     }
+   *   }-&#42;/;
+   * </pre>   
+   * 
+   * @see Log#warn(String)
+   * @see Log#warn(String, Throwable)
+   * 
+   * @param message the text to be logged
+   * @param e the native JavaScript exception object
+   */
   public static void warn(String message, JavaScriptObject e) {
     impl.warn(message, e);
   }
 
+  /**
+   * Log a <code>WARN</code> level message from within
+   * a Java try/catch block, e.g.
+   * <pre>
+   *   private native void javaTryCatchExample() {
+   *     try {
+   *       throw new RuntimeException();
+   *     } catch(e) {
+   *       Log.warn("Caught Java Exception", e);
+   *     }
+   *   }
+   * </pre>   
+   * 
+   * @see Log#warn(String)
+   * @see Log#warn(String, Throwable)
+   * 
+   * @param message the text to be logged
+   * @param e the native JavaScript exception object
+   */
   public static void warn(String message, Throwable e) {
     impl.warn(message, e);
   }
