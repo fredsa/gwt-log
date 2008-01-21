82cd32f5bff09d7a346ad538ff79c9378bb2fc0f
Fred Sauer <fredsa@gmail.com>
2008-01-21 06:23:27 +0000

    Use GWT14_HOME in *.cmd
    git-svn-id: https://gwt-log.googlecode.com/svn/trunk@83 dec3d7ae-b23b-0410-ba11-938393925c7a

--- Log/LogDemo-compile.cmd
+++ Log/LogDemo-compile.cmd
@@ -1,1 +1,1 @@
-@java -cp "%~dp0\src;%~dp0\demo;%~dp0\bin;%GWT_HOME%/gwt-user.jar;%GWT_HOME%/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" -style PRETTY %* com.allen_sauer.gwt.log.demo.LogDemo
+@java -cp "%~dp0\src;%~dp0\demo;%~dp0\bin;%GWT14_HOME%/gwt-user.jar;%GWT14_HOME%/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" -style PRETTY %* com.allen_sauer.gwt.log.demo.LogDemo
