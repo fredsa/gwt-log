0ed764190afde7bdf6f454b37a176eaaaebd8837
Fred Sauer <fredsa@gmail.com>
2008-01-29 20:28:44 +0000

    Add RemoteLogger
    git-svn-id: https://gwt-log.googlecode.com/svn/trunk/Log@124 dec3d7ae-b23b-0410-ba11-938393925c7a

---
+++ src/com/allen_sauer/gwt/log/client/RemoteLogger.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2008 Fred Sauer
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.allen_sauer.gwt.log.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+
+public class RemoteLogger extends AbstractLogger {
+  private RemoteLoggerServiceAsync service;
+  private AsyncCallback callback;
+  private boolean failed = false;
+
+  public RemoteLogger() {
+    service = (RemoteLoggerServiceAsync) GWT.create(RemoteLoggerService.class);
+    final ServiceDefTarget target = (ServiceDefTarget) service;
+    target.setServiceEntryPoint(GWT.getModuleBaseURL() + "gwt-log");
+
+    callback = new AsyncCallback() {
+      public void onFailure(Throwable ex) {
+        if (!failed) {
+          failed = true;
+          Log.fatal("Remote logging will be suspended due to communication failure with "
+              + GWT.getTypeName(service) + " at " + target.getServiceEntryPoint(), ex);
+        }
+      }
+
+      public void onSuccess(Object result) {
+      }
+    };
+  }
+
+  public void debug(String message, Throwable throwable) {
+    if (!failed) {
+      service.debug(message, throwable, callback);
+    }
+  }
+
+  public void error(String message, Throwable throwable) {
+    if (!failed) {
+      service.error(message, throwable, callback);
+    }
+  }
+
+  public void fatal(String message, Throwable throwable) {
+    if (!failed) {
+      service.fatal(message, throwable, callback);
+    }
+  }
+
+  public void info(String message, Throwable throwable) {
+    if (!failed) {
+      service.info(message, throwable, callback);
+    }
+  }
+
+  public final boolean isSupported() {
+    return true;
+  }
+
+  public void warn(String message, Throwable throwable) {
+    if (!failed) {
+      service.warn(message, throwable, callback);
+    }
+  }
+
+  void log(int logLevel, String message) {
+    assert false;
+    // Method never called
+  }
+}
---
+++ src/com/allen_sauer/gwt/log/server/RemoteLoggerServiceImpl.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2008 Fred Sauer
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.allen_sauer.gwt.log.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import com.allen_sauer.gwt.log.client.Log;
+import com.allen_sauer.gwt.log.client.RemoteLoggerService;
+
+public class RemoteLoggerServiceImpl extends RemoteServiceServlet implements RemoteLoggerService {
+  public void debug(String message, Throwable ex) {
+    try {
+      Log.debug(message, ex);
+    } catch (RuntimeException e) {
+      System.err.println("Failed to log message due to " + e.toString());
+      e.printStackTrace();
+    }
+  }
+
+  public void error(String message, Throwable ex) {
+    try {
+      Log.error(message, ex);
+    } catch (RuntimeException e) {
+      System.err.println("Failed to log message due to " + e.toString());
+      e.printStackTrace();
+    }
+  }
+
+  public void fatal(String message, Throwable ex) {
+    try {
+      Log.fatal(message, ex);
+    } catch (RuntimeException e) {
+      System.err.println("Failed to log message due to " + e.toString());
+      e.printStackTrace();
+    }
+  }
+
+  public void info(String message, Throwable ex) {
+    try {
+      Log.info(message, ex);
+    } catch (RuntimeException e) {
+      System.err.println("Failed to log message due to " + e.toString());
+      e.printStackTrace();
+    }
+  }
+
+  public void warn(String message, Throwable ex) {
+    try {
+      Log.warn(message, ex);
+    } catch (RuntimeException e) {
+      System.err.println("Failed to log message due to " + e.toString());
+      e.printStackTrace();
+    }
+  }
+}
