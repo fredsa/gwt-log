0b99d1ce4787cfc5e5a7575a0f953a8ef86f6bd3
Fred Sauer <fredsa@gmail.com>
2008-03-21 18:55:31 +0000

    trunk is now GWT 1.5
    git-svn-id: https://gwt-log.googlecode.com/svn/trunk@183 dec3d7ae-b23b-0410-ba11-938393925c7a

--- Log/LogDemo-compile.cmd
+++ Log/LogDemo-compile.cmd
@@ -1,1 +1,1 @@
-@java -cp "%~dp0\src;%~dp0\demo;%~dp0\bin;%GWT14_HOME%/gwt-user.jar;%GWT14_HOME%/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" -style PRETTY %* com.allen_sauer.gwt.log.demo.LogDemo
+@java -cp "%~dp0\src;%~dp0\demo;%~dp0\bin;%GWT15_HOME%/gwt-user.jar;%GWT15_HOME%/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" -style PRETTY %* com.allen_sauer.gwt.log.demo.LogDemo
