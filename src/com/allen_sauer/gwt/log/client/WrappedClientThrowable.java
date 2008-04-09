e588e0f9f9d71760940a573c0406779adb4a21e1
Fred Sauer <fredsa@gmail.com>
2008-04-09 05:42:26 +0000

    Wrap/unwrap client thrown exceptions to return RPC surface area
    git-svn-id: https://gwt-log.googlecode.com/svn/trunk/Log@192 dec3d7ae-b23b-0410-ba11-938393925c7a

---
+++ src/com/allen_sauer/gwt/log/client/WrappedClientThrowable.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2008 Fred Sauer
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.allen_sauer.gwt.log.client;
+
+import java.io.Serializable;
+
+/**
+ * Representation of a client-side thrown exception, which can be serialized via RPC.
+ */
+@SuppressWarnings("serial")
+public class WrappedClientThrowable implements Serializable {
+  // CHECKSTYLE_JAVADOC_OFF
+
+  private ClientStackTraceElement[] clientStackTrace;
+  private String message;
+  private String originalClassName;
+
+  WrappedClientThrowable(Throwable ex) {
+    if (ex != null) {
+      originalClassName = ex.getClass().getName();
+      message = ex.getMessage();
+      StackTraceElement[] stackTrace = ex.getStackTrace();
+      clientStackTrace = new ClientStackTraceElement[stackTrace.length];
+      for (int i = 0; i < stackTrace.length; i++) {
+        clientStackTrace[i] = new ClientStackTraceElement(stackTrace[i].getClassName(),
+            stackTrace[i].getMethodName(), stackTrace[i].getFileName(),
+            stackTrace[i].getLineNumber());
+      }
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private WrappedClientThrowable() {
+  }
+
+  public ClientStackTraceElement[] getClientStackTrace() {
+    return clientStackTrace;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public String getOriginalClassName() {
+    return originalClassName;
+  }
+}
