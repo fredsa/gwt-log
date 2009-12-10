19cdfd29f82e974be0dfdc4642745332665bf1c2
Fred Sauer <fredsa@gmail.com>
2009-12-10 19:15:05 +0000

    Add back stack traces to ConsoleLogger/FirebugLogger which were accidentally factored out in the last few commits.
    Found by: Ehab
    
    git-svn-id: https://gwt-log.googlecode.com/svn/trunk@440 dec3d7ae-b23b-0410-ba11-938393925c7a

--- Log/src/com/allen_sauer/gwt/log/client/ConsoleLogger.java
+++ Log/src/com/allen_sauer/gwt/log/client/ConsoleLogger.java
@@ -1,23 +1,21 @@
 /*
  * Copyright 2009 Fred Sauer
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.allen_sauer.gwt.log.client;
 
 /**
  * Logger which sends output via <code>$wnd.console.log()</code> if <code>$wnd.console.log</code> is
- * a function.
+ * a function, unless <code>$wnd.console.firebug</code> is defined.
  */
 public final class ConsoleLogger implements Logger {
   // CHECKSTYLE_JAVADOC_OFF
@@ -31,7 +29,11 @@ public final class ConsoleLogger implements Logger {
   }-*/;
 
   public void log(LogRecord record) {
-    logMessage(record.getFormattedMessage());
+    // Haven't figured out how to get line breaks to interpret inside of calls to $wnd.console.log()
+    String[] lines = (record.getFormattedMessage() + LogUtil.stackTraceToString(record.getThrowable())).split("\n");
+    for (int i = 0; i < lines.length; i++) {
+      logMessage(lines[i]);
+    }
   }
 
   public void setCurrentLogLevel(int level) {
--- Log/src/com/allen_sauer/gwt/log/client/FirebugLogger.java
+++ Log/src/com/allen_sauer/gwt/log/client/FirebugLogger.java
@@ -1,26 +1,23 @@
 /*
  * Copyright 2009 Fred Sauer
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.allen_sauer.gwt.log.client;
 
 /**
- * Logger which sends output via <a href="http://www.getfirebug.com/">Firebug</a>
- * or <a href="http://www.getfirebug.com/lite.html">Firebug Lite</a>
- * via <code>$wnd.console.debug()</code>, <code>$wnd.console.info()</code>,
- * <code>$wnd.console.warn()</code> and <code>$wnd.console.error()</code> if
- * <code>$wnd.console.firebug</code> is defined.
+ * Logger which sends output via <a href="http://www.getfirebug.com/">Firebug</a> or <a
+ * href="http://www.getfirebug.com/lite.html">Firebug Lite</a> via <code>$wnd.console.debug()</code>
+ * , <code>$wnd.console.info()</code>, <code>$wnd.console.warn()</code> and
+ * <code>$wnd.console.error()</code> if <code>$wnd.console.firebug</code> is defined.
  */
 public final class FirebugLogger implements Logger {
   // CHECKSTYLE_JAVADOC_OFF
@@ -34,7 +31,10 @@ public final class FirebugLogger implements Logger {
   }-*/;
 
   public void log(LogRecord record) {
-    logMessage(record.getLevel(), record.getFormattedMessage());
+    String[] lines = (record.getFormattedMessage() + LogUtil.stackTraceToString(record.getThrowable())).split("\n");
+    for (int i = 0; i < lines.length; i++) {
+      logMessage(record.getLevel(), lines[i]);
+    }
   }
 
   public void setCurrentLogLevel(int level) {
