55bb33c4810f84f7fbf97e8a7d00f078a6afe9e3
Fred Sauer <fredsa@google.com>
2010-07-12 22:47:31 +0000

    Add Logging of X-Forwarded-For headers for RemoteLogger as suggested in issue 31
    Suggested by: kmj7777
    
    git-svn-id: https://gwt-log.googlecode.com/svn/trunk@499 dec3d7ae-b23b-0410-ba11-938393925c7a

--- Log/src/com/allen_sauer/gwt/log/server/RemoteLoggerServiceImpl.java
+++ Log/src/com/allen_sauer/gwt/log/server/RemoteLoggerServiceImpl.java
@@ -1,14 +1,16 @@
 /*
  * Copyright 2009 Fred Sauer
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.allen_sauer.gwt.log.server;
@@ -29,12 +31,21 @@ import javax.servlet.http.HttpServletRequest;
 @SuppressWarnings("serial")
 public class RemoteLoggerServiceImpl extends RemoteServiceServlet implements RemoteLoggerService {
 
+  /**
+   * Non-RFC standard header. See http://en.wikipedia.org/wiki/X-Forwarded-For
+   */
+  private static final String X_FORWARDED_FOR = "X-Forwarded-For";
+
   public final void log(ArrayList<LogRecord> logRecords) {
     for (Iterator<LogRecord> iterator = logRecords.iterator(); iterator.hasNext();) {
       LogRecord record = iterator.next();
       try {
         HttpServletRequest request = getThreadLocalRequest();
         record.set("remoteAddr", request.getRemoteAddr());
+        String xForwardedFor = request.getHeader(X_FORWARDED_FOR);
+        if (xForwardedFor != null) {
+          record.set(X_FORWARDED_FOR, xForwardedFor);
+        }
         Log.log(record);
       } catch (RuntimeException e) {
         System.err.println("Failed to log message due to " + e.toString());
