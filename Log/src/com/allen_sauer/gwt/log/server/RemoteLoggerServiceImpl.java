8fa5b8ecdb932f6a9941b66d8dfe7ad180b53663
Fred Sauer <fredsa@google.com>
2011-01-23 01:07:48 +0000

    Detect missing symbolMaps init-param and warn developer on server logs
    git-svn-id: https://gwt-log.googlecode.com/svn/trunk@551 dec3d7ae-b23b-0410-ba11-938393925c7a

--- Log/src/com/allen_sauer/gwt/log/server/RemoteLoggerServiceImpl.java
+++ Log/src/com/allen_sauer/gwt/log/server/RemoteLoggerServiceImpl.java
@@ -1,16 +1,14 @@
 /*
  * Copyright 2009 Fred Sauer
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.allen_sauer.gwt.log.server;
@@ -48,10 +46,18 @@ public class RemoteLoggerServiceImpl extends RemoteServiceServlet implements Rem
   // TODO Replace this class with the standard GWT version in GWT 2.2
   private StackTraceDeobfuscator deobfuscator;
 
+  @SuppressWarnings("deprecation")
   @Override
   public void init(ServletConfig config) throws ServletException {
     super.init(config);
     String symbolMaps = config.getInitParameter(PARAMETER_SYMBOL_MAPS);
+    if (symbolMaps == null) {
+      Log.diagnostic(
+          "In order to enable stack trace deobfuscation, please specify the '"
+              + PARAMETER_SYMBOL_MAPS + "' <init-param> for the "
+              + RemoteLoggerServiceImpl.class.getName() + " servlet in your web.xml", null);
+      return;
+    }
     deobfuscator = new StackTraceDeobfuscator(symbolMaps);
   }
 
