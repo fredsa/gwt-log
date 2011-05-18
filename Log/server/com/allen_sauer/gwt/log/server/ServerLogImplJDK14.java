65c0be21fb2cc89dec30a39456894057878738c9
Fred Sauer <fredsa@google.com>
2011-05-18 18:11:32 +0000

    gwt-log's DEBUG/TRACE levels were mapped to java.util.logging's FINE/FINEST levels; now using CONFIG/FINE instead
    git-svn-id: https://gwt-log.googlecode.com/svn/trunk@612 dec3d7ae-b23b-0410-ba11-938393925c7a

--- Log/server/com/allen_sauer/gwt/log/server/ServerLogImplJDK14.java
+++ Log/server/com/allen_sauer/gwt/log/server/ServerLogImplJDK14.java
@@ -1,16 +1,14 @@
 /*
  * Copyright 2009 Fred Sauer
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.allen_sauer.gwt.log.server;
@@ -48,7 +46,7 @@ public final class ServerLogImplJDK14 implements ServerLog {
   }
 
   public boolean isDebugEnabled() {
-    return logger.isLoggable(Level.FINE);
+    return logger.isLoggable(Level.CONFIG);
   }
 
   public boolean isErrorEnabled() {
@@ -68,7 +66,7 @@ public final class ServerLogImplJDK14 implements ServerLog {
   }
 
   public boolean isTraceEnabled() {
-    return logger.isLoggable(Level.FINEST);
+    return logger.isLoggable(Level.FINE);
   }
 
   public boolean isWarnEnabled() {
@@ -91,9 +89,9 @@ public final class ServerLogImplJDK14 implements ServerLog {
   private Level mapGWTLogLevelToImplLevelObject(int gwtLogLevel) {
     switch (gwtLogLevel) {
       case Log.LOG_LEVEL_TRACE:
-        return Level.FINEST;
-      case Log.LOG_LEVEL_DEBUG:
         return Level.FINE;
+      case Log.LOG_LEVEL_DEBUG:
+        return Level.CONFIG;
       case Log.LOG_LEVEL_INFO:
         return Level.INFO;
       case Log.LOG_LEVEL_WARN:
